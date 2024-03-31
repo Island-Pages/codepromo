a1fca81074219e10eee23c0b8e9b6813dac8c510
higorxi <higorgiovane@ucashapp.com>
2024-03-30 22:26:52 -0300

    new login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,20 +1,35 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from 'react';
-import { Container, Typography, TextField, Button, CircularProgress } from '@mui/material';
 import { login } from '../services/AuthService';
+import { CircularProgress } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import logo from '../assets/logo.jpeg'
 
-const Login = () => {
+const defaultTheme = createTheme();
+
+export default function SignIn() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [loading, setLoading] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async () => {
     try {
       setLoading(true);
-      const response =  await login(email, senha);
-      if(response){
-        window.location.reload()
+      const response = await login(email, senha);
+      if (response) {
+        window.location.reload();
       }
-     
     } catch (error) {
       console.error('erro ao fazer login', error);
     } finally {
@@ -23,65 +38,88 @@ const Login = () => {
   };
 
   return (
-    <Container
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100vw',
-        height: '100vh',
-        background: 'linear-gradient(to left, #203b62, #0f2646)',
-      }}
-    >
-      <div
-        style={{
+    <ThemeProvider theme={defaultTheme}>
+      <Box
+        sx={{
+          background: 'linear-gradient(to right, #2196f3, #0d47a1)',
+          minHeight: '100vh',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-          gap: '24px',
-          padding: '32px',
-          border: '1px solid #ccc',
-          borderRadius: '10px',
-          backgroundColor: '#f7f7f7', 
+          justifyContent: 'center',
         }}
       >
-        <Typography variant="h5" gutterBottom>
-          Faça login
-        </Typography>
-        <img style={{ width: '150px' }} src="src\assets\image.png" alt="Logo" />
-        <TextField
-          style={{ width: '300px', maxWidth: '100%' }}
-          label="Email"
-          variant="outlined"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          style={{ width: '300px', maxWidth: '100%' }}
-          label="Senha"
-          variant="outlined"
-          type="senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-        />
-        <Button
-          style={{
-            padding: '16px 32px',
-            backgroundColor: '#007bff',
-            color: '#fff',
-            borderRadius: '5px',
-            cursor: 'pointer',
-            width: '300px',
-            maxWidth: '100%',
-          }}
-          variant="contained"
-          onClick={handleSubmit}
-        >
-          {loading ? <CircularProgress color="inherit" size={24} /> : 'Login'}
-        </Button>
-      </div>
-    </Container>
+        <CssBaseline />
+        <Container component="main" maxWidth="xs">
+          <Box
+            sx={{
+              backgroundColor: 'white',
+              padding: '20px',
+              borderRadius: '10px',
+              width: '100%',
+              boxSizing: 'border-box',
+              minHeight: '50vh',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              boxShadow: 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
+            }}
+          >
+            <Avatar sx={{ m: 1, height: '35%', width: '35%'}}>
+              <img src={logo} alt="Lock" style={{ width: '100%', height: 'auto' }} />
+            </Avatar>
+            <Box sx={{ mt: 1, width: '100%' }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="senha"
+                label="Senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                type={showPassword ? 'text' : 'senha'}
+                id="senha"
+                autoComplete="current-password"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={(e) => e.preventDefault()}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handleSubmit}
+              >
+                {loading ? <CircularProgress color="inherit" size={24} /> : 'Login'}
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
-};
-
-export default Login;
+}
